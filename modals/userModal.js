a3afe86250de22ff6119b60a1bac8e538d06ecd4
Banti4750 <bantikumar6203818460@gmail.com>
2025-08-20 04:45:01 +0530

    added users modal

---
+++ modals/userModal.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+    {
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+            maxlength: 20
+        },
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        dob: {
+            type: Date,
+        },
+        gender: {
+            type: String,
+            enum: ["Male", "Female", "Other"],
+            default: "Other",
+        },
+        profileImage: {
+            type: String,
+            default: "https://via.placeholder.com/150",
+        },
+    },
+    { timestamps: true }
+);
+
+// Create model
+const Users = mongoose.model("Users", userSchema);
+
+export default Users;
