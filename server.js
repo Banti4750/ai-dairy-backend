69290d6d3dea32f6a4e81264860dff52a6c140cf
Banti4750 <bantikumar6203818460@gmail.com>
2025-08-20 04:27:57 +0530

    conneted to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDb from './config/connectDB.js';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+connectDb();
 
 app.listen(PORT, () => {
     console.log(`✅ Server is listenting on port ${PORT}`)
