d8526e209ae3d7189cd33343c4f00849733ef258
Banti4750 <bantikumar6203818460@gmail.com>
2025-08-20 05:47:35 +0530

    added login api

--- routes/usersRoutes/authRoutes.js
+++ routes/usersRoutes/authRoutes.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import Users from '../../modals/userModal.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const router = express.Router();
 
 
@@ -40,6 +44,47 @@ router.post('/register', async (req, res) => {
 
     } catch (error) {
         console.error("Register error:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+//login user
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "All fields required"
+            })
+        }
+
+        if (password.length < 6 || password.length > 20) {
+            return res.status(400).json({ message: "Password must be at least 6 characters and less than 20" });
+        }
+
+        const isUserExit = await Users.findOne({ email });
+        if (!isUserExit) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        const isMatch = await bcrypt.compare(password, isUserExit.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        const token = jwt.sign(
+            { id: isUserExit._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" } // token valid for 1 hour
+        );
+
+        res.status(200).json({
+            message: "User login successfully",
+            token
+        })
+
+    } catch (error) {
+        console.error("Login error:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 })
